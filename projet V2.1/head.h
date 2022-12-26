93dee5163f4ce38b70d49ff8207466be2a0001a9
Blazarothe <120842830+Blazarothe@users.noreply.github.com>
2022-12-26 15:48:17 +0100

    Add files via upload

---
+++ projet V2.1/head.h
@@ -0,0 +1,14 @@
+//
+// Created by charv on 21/12/2022.
+//
+
+#ifndef TEST3_HEAD_H
+#define TEST3_HEAD_H
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+int zebi();
+int remplir_les_cases(int i,  int l, int c,int random_p,int random_s);
+void piece();
+
+#endif //TEST3_HEAD_H
